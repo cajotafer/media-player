3c7d7e93e60aba67d9b9993e9ea6422da9c7d653
Cajotafer <hello@cajotafer.com>
2019-09-27 11:07:50 -0500

    [UPDATE] AutoPlay -> TypeScript

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -5,19 +5,8 @@ class MediaPlayer {
     this._initPlugins();
   }
   _initPlugins() {
-    const player = {
-      play: () => this.play(),
-      pause: () => this.pause(),
-      media: this.media,
-      get muted() {
-        return this.media.muted;
-      },
-      set muted(value) {
-        this.media.muted = value;
-      }
-    };
     this.plugins.forEach(plugin => {
-      plugin.run(player);
+      plugin.run(this);
     });
   }
   play() {
--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,5 @@
 import MediaPlayer from "./MediaPlayer.js";
-import AutoPlay from "./plugins/AutoPlay.js";
+import AutoPlay from "./plugins/AutoPlay.ts";
 import AutoPause from "./plugins/AutoPause.ts";
 
 const $video = document.querySelector("video");
