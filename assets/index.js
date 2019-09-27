85f8d99bd49814a247d46ba333c4fe8c3e73ae7a
Cajotafer <hello@cajotafer.com>
2019-09-27 02:52:39 -0500

    [UPDATE] - Using setter and getter in _initPlugins

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,14 @@
 import MediaPlayer from "./MediaPlayer.js";
 import AutoPlay from "./plugins/AutoPlay.js";
 
-const video = document.querySelector("video");
-const plugins = [new AutoPlay()];
-const player = new MediaPlayer({ el: video, plugins });
+const $video = document.querySelector("video");
+const plugins = [
+  /* new AutoPlay() */
+];
+const player = new MediaPlayer({ el: $video, plugins });
 
-const button = document.querySelector("button");
-button.onclick = () => player.togglePlay();
+const $playButton = document.querySelector(".play");
+$playButton.onclick = () => player.togglePlay();
+
+const $muteButton = document.querySelector(".mute");
+$muteButton.onclick = () => player.toggleMute();
